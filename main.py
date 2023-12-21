5d33c28b616d3dbbaf938a7fece9fabf58865de8
kolad <koladik@gmail.com>
2023-12-21 21:19:50 +0300

    Branch commint 1

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
 import sys
+from typing import Any
+
+from cv2 import Mat, UMat
+from numpy import ndarray, dtype, generic
+
 import PathCreator
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,11 +24,33 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = img[0:2 ** 10 - 1, 0:2 ** 10 - 1]
 
 
-def get_zeromarkers(area_bg):
+def get_marker_from_background(img, area_bg):
       area_dist = cv2.distanceTransform(area_bg, cv2.DIST_L2, 0)
-      area_dist = np.uint8(area_dist / area_dist.max() * 255)
-      ret, area_sure = cv2.threshold(area_dist, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-      ret, area_marks = cv2.connectedComponents(area_sure)
+      area_dist = np.uint8((area_dist / area_dist.max()) * 255)
+      ret, area_su = cv2.threshold(area_dist, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+      ret, area_marks = cv2.connectedComponents(area_su)
+      area_unknown = cv2.subtract(area_bg, area_su)
+      area_marks = area_marks + 1
+      area_marks[area_unknown == 255] = 0
+      area_marks = cv2.watershed(img, area_marks)
+
+      fig = plt.figure(figsize=(10, 10))
+      ax = [fig.add_subplot(2, 2, 1),
+            fig.add_subplot(2, 2, 2),
+            fig.add_subplot(2, 2, 3),
+            fig.add_subplot(2, 2, 4)]
+      ax[0].imshow(cv2.merge((area_bg, area_bg, area_bg)))
+      ax[1].imshow(cv2.merge((area_su, area_su, area_su)))
+      ax[2].imshow(cv2.merge((area_dist, area_dist, area_dist)))
+      ax[3].imshow(area_marks)
+      ax[0].axis('off')
+      ax[1].axis('off')
+      ax[2].axis('off')
+      ax[3].axis('off')
+      ax[0].set_title("background")
+      ax[1].set_title("sure foreground")
+      plt.show()
+
       return area_marks
 
 def GetMarkerArea(img):
@@ -31,25 +58,44 @@ def GetMarkerArea(img):
       l_c, a, b = cv2.split(lab)
       l_c = cv2.bilateralFilter(l_c, 15, 40, 80)
       area_bg = 255 - cv2.Canny(l_c, 100, 200)
-      area_marks = get_zeromarkers(area_bg)
-      area_su = np.uint8(area_marks != -1)*255
-      print(area_su.max())
-      print(area_bg.max())
-      area_unknown = cv2.subtract(area_bg, area_su)
-      area_marks = area_marks + 1
-      area_marks[area_unknown == 255] = 0
-      return cv2.watershed(img, area_marks)
+      kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+      area_bg = cv2.morphologyEx(area_bg, cv2.MORPH_OPEN, kernel, iterations=1)
+      area_marks0 = get_marker_from_background(img, area_bg)
+
+      area_marks = area_marks0
+      area_marks[np.logical_and(area_marks != 1, area_marks != 0)] = 255
+      area_bg1 = cv2.absdiff(area_marks.astype(np.uint8), area_bg)
+      area_marks = get_marker_from_background(img, area_bg1)
+      print(area_bg.dtype)
+      print(np.max(area_bg1))
+      print(np.max(area_bg1))
+      fig = plt.figure(figsize=(10, 10))
+      ax = [fig.add_subplot(2, 2, 1),
+            fig.add_subplot(2, 2, 2),
+            fig.add_subplot(2, 2, 3),
+            fig.add_subplot(2, 2, 4)]
+      ax[0].imshow(area_marks0)
+      ax[1].imshow(cv2.merge((area_bg, area_bg, area_bg)))
+      ax[2].imshow(cv2.merge((area_bg1, area_bg1, area_bg1)))
+      ax[3].imshow(area_marks)
+      ax[0].axis('off')
+      ax[1].axis('off')
+      ax[2].axis('off')
+      ax[3].axis('off')
+      ax[0].set_title("markers iteration 1")
+      ax[1].set_title("sure foreground")
+      plt.show()
+
+      return area_marks
 
 
 #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 #l_c_bg = cv2.morphologyEx(l_c_bg, cv2.MORPH_OPEN, kernel, iterations=3)
 # Marker labelling
 
-
 area_marks = GetMarkerArea(img)
-# l_c_marks2 = cv2.watershed(img,l_c_marks)
-# img2 = cv2.merge((l_c,l_c,l_c))
-# img[l_c_marks2 == -1] = [255,0,0]
+exit()
+
 
 masks = []
 for i in range(area_marks.max()):
